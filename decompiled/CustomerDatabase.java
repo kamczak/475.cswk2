c4e5e37b57cbf60985475cbf367687b9e06eeefe
Kamil Cichy <kamczak@gmail.com>
2011-11-26 15:28:00 +0000

    extension renaming

---
+++ decompiled/CustomerDatabase.java
@@ -0,0 +1,13 @@
+// Decompiled by DJ v3.12.12.96 Copyright 2011 Atanas Neshkov  Date: 2011-11-26 15:16:26
+// Home Page: http://members.fortunecity.com/neshkov/dj.html  http://www.neshkov.com/dj.html - Check often for new version!
+// Decompiler options: packimports(3) 
+
+package com.acmetelecom.customer;
+
+import java.util.List;
+
+public interface CustomerDatabase
+{
+
+    public abstract List getCustomers();
+}
