d44914d2aa8a8e0ce7950ff263ffd0d9e7446ea1
cupofcat <cupofcat@gmail.com>
2011-11-28 14:42:40 +0000

    Improved tests a bit.

--- test/com/acmetelecom/BillingSystemTest.java
+++ test/com/acmetelecom/BillingSystemTest.java
@@ -88,7 +88,7 @@ public class BillingSystemTest {
 
     @SuppressWarnings("unchecked")
     @Test
-    public void calculatesPeakCallCorrectly() {
+    public void calculatesWholeInPeakCallCorrectly() {
 	final Calendar start = Calendar.getInstance();
 	start.clear();
 	start.set(2011, 0, 1, 8, 0, 0);
@@ -120,29 +120,19 @@ public class BillingSystemTest {
 
     @SuppressWarnings("unchecked")
     @Test
-    public void calculatesOverlappingPeakCallCorrectly() {
-	final Calendar start1 = Calendar.getInstance();
-	start1.clear();
-	start1.set(2011, 0, 1, 6, 0, 0);
+    public void calculatesOverlappingLeftPeakCallCorrectly() {
+	final Calendar start = Calendar.getInstance();
+	start.clear();
+	start.set(2011, 0, 1, 6, 0, 0);
 
-	final Calendar end1 = Calendar.getInstance();
-	end1.clear();
-	end1.set(2011, 0, 1, 8, 0, 0);
-
-	final Calendar start2 = Calendar.getInstance();
-	start2.clear();
-	start2.set(2011, 0, 1, 18, 0, 0);
-
-	final Calendar end2 = Calendar.getInstance();
-	end2.clear();
-	end2.set(2011, 0, 1, 20, 0, 0);
+	final Calendar end = Calendar.getInstance();
+	end.clear();
+	end.set(2011, 0, 1, 8, 0, 0);
 
 	context.checking(new Expectations() {{
 	    allowing (clock).getCurrentTime();
-	    		will(onConsecutiveCalls(returnValue(start1.getTimeInMillis()),
-		    				returnValue(end1.getTimeInMillis()),
-		    				returnValue(start2.getTimeInMillis()),
-		    				returnValue(end2.getTimeInMillis())
+	    		will(onConsecutiveCalls(returnValue(start.getTimeInMillis()),
+		    				returnValue(end.getTimeInMillis())
 		    				));
 
 	    allowing (tariffDB).tarriffFor(firstCustomer); will(returnValue(Tariff.Standard));
@@ -151,11 +141,72 @@ public class BillingSystemTest {
 	    
 	    oneOf (billGenerator).send(with(same(firstCustomer)),
 		    		       with(aNonNull(List.class)),
-		    		       with(equal("72.00")));
+		    		       with(equal("36.00")));
 	}});
 
 	billingSystem.callInitiated(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
 	billingSystem.callCompleted(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
+
+	billingSystem.createCustomerBills();
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void calculatesOverlappingRightPeakCallCorrectly() {
+	final Calendar start = Calendar.getInstance();
+	start.clear();
+	start.set(2011, 0, 1, 18, 0, 0);
+
+	final Calendar end = Calendar.getInstance();
+	end.clear();
+	end.set(2011, 0, 1, 20, 0, 0);
+
+	context.checking(new Expectations() {{
+	    allowing (clock).getCurrentTime();
+	    		will(onConsecutiveCalls(returnValue(start.getTimeInMillis()),
+		    				returnValue(end.getTimeInMillis())
+		    				));
+
+	    allowing (tariffDB).tarriffFor(firstCustomer); will(returnValue(Tariff.Standard));
+	    
+	    allowing (customerDB).getCustomers(); will(returnValue(CUSTOMERS));
+	    
+	    oneOf (billGenerator).send(with(same(firstCustomer)),
+		    		       with(aNonNull(List.class)),
+		    		       with(equal("36.00")));
+	}});
+
+	billingSystem.callInitiated(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
+	billingSystem.callCompleted(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
+
+	billingSystem.createCustomerBills();
+    }
+    
+    @SuppressWarnings("unchecked")
+    @Test
+    public void calculatesOverlappingBothPeakCallCorrectly() {
+	final Calendar start = Calendar.getInstance();
+	start.clear();
+	start.set(2011, 0, 1, 6, 0, 0);
+
+	final Calendar end = Calendar.getInstance();
+	end.clear();
+	end.set(2011, 0, 1, 20, 0, 0);
+
+	context.checking(new Expectations() {{
+	    allowing (clock).getCurrentTime();
+	    		will(onConsecutiveCalls(returnValue(start.getTimeInMillis()),
+		    				returnValue(end.getTimeInMillis())
+		    				));
+
+	    allowing (tariffDB).tarriffFor(firstCustomer); will(returnValue(Tariff.Standard));
+	    
+	    allowing (customerDB).getCustomers(); will(returnValue(CUSTOMERS));
+	    
+	    oneOf (billGenerator).send(with(same(firstCustomer)),
+		    		       with(aNonNull(List.class)),
+		    		       with(equal("252.00")));
+	}});
 
 	billingSystem.callInitiated(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
 	billingSystem.callCompleted(FIRST_CUSTOMER_NUMBER, OTHER_NUMBER);
