9f072fd11e9ac109cf07d86123a1918b0e757b8e
ryszardkaleta <ryszardkaleta@gmail.com>
2011-12-03 18:32:33 +0000

    NewStrategy tak na szybko, trzeba przetestowac!

--- src/com/acmetelecom/strategy/NewStrategy.java
+++ src/com/acmetelecom/strategy/NewStrategy.java
@@ -1,8 +1,10 @@
 package com.acmetelecom.strategy;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
 
-import org.joda.time.Period;
+import org.joda.time.Interval;
 
 import com.acmetelecom.bill.DaytimePeakPeriod;
 import com.acmetelecom.bill.Strategy;
@@ -14,8 +16,19 @@ public class NewStrategy implements Strategy {
 	@Override
 	public BigDecimal getCost(Tariff tariff, Call call) {
 		BigDecimal cost = null;
-//		Period peakPeriod = new DaytimePeakPeriod().getPeakTimePeriod();
+
+		Interval callInterval = new Interval(call.startDateTime(), call.endDateTime());
+		List<Interval> peakIntervals = new DaytimePeakPeriod().getPeakTimeIntervals(callInterval);
 		
+		int peakDuration = 0;
+		for (Interval peakTime : peakIntervals) {
+			peakDuration += (int) callInterval.overlap(peakTime).toDuration().getStandardSeconds();
+		}
+		
+		int offPeakDuration = (int) callInterval.toDuration().getStandardSeconds() - peakDuration;
+		
+		cost = new BigDecimal(peakDuration).multiply(tariff.peakRate()).add(new BigDecimal(offPeakDuration).multiply(tariff.offPeakRate()));
+		cost = cost.setScale(0, RoundingMode.HALF_UP);
 		return cost;
 	}
 
