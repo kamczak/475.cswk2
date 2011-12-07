6451b9f8c2e522167ecdb6f8d7ab3cf254146b1b
cupofcat <cupofcat@gmail.com>
2011-12-07 15:20:53 +0000

    Changed MoneyFormatter to package private

--- src/com/acmetelecom/bill/MoneyFormatter.java
+++ src/com/acmetelecom/bill/MoneyFormatter.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 /**
  * Small utility converting pounds to pence in a fixed format
  */
-public class MoneyFormatter {
+class MoneyFormatter {
     public static String penceToPounds(BigDecimal pence) {
         BigDecimal pounds = pence.divide(new BigDecimal(100));
         return String.format("%.2f", pounds.doubleValue());
